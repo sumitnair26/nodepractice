27405a64a85f2f013e53d60a60974083a7d61763
Sumit Nair <sumitnair@Sumits-MacBook-Pro.local>
2023-03-21 21:10:59 +0530

    Module and require

---
+++ modules.js
@@ -0,0 +1,8 @@
+/* Module And Require */
+
+// Destructuring object
+const { people, ages} = require('./people');
+
+
+
+console.log(people, ages);
---
+++ people.js
@@ -0,0 +1,8 @@
+const people = ['sumit', 'anju', 'abhi', 'anu', 'ajju'];
+const ages = [20,30,32,35];
+
+console.log(people);
+
+module.exports = {
+    people, ages
+};
