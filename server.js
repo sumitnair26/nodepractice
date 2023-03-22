ee2d4bfe0e8d0b19b9b58e38754eae1517d0bd35
Sumit Nair <sumitnair@Sumits-MacBook-Pro.local>
2023-03-22 21:25:38 +0530

    request and response

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 const http = require('http');
 
 const server = http.createServer((req, res)=>{
-    console.log('request made');
+    console.log(req.url, req.method);
+    //set header content type
+    res.setHeader('Content-Type','text/html');
+
+    res.write('<p>Hello on landing page</p>');
+    res.write('<p>Hello again on landing page</p>');
+    res.end();
 });
 
 server.listen(3000, 'localhost',()=>{
