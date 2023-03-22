eeae08e7fad4f82c65328b12823948a81ac2e39c
Sumit Nair <sumitnair@Sumits-MacBook-Pro.local>
2023-03-22 21:59:14 +0530

    Adding HTML Page

--- server.js
+++ server.js
@@ -1,13 +1,24 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res)=>{
     console.log(req.url, req.method);
     //set header content type
-    res.setHeader('Content-Type','text/html');
+     res.setHeader('Content-Type','text/html');
+    // res.write('<p>Hello on landing page</p>');
+    // res.write('<p>Hello again on landing page</p>');
+    // res.end();
 
-    res.write('<p>Hello on landing page</p>');
-    res.write('<p>Hello again on landing page</p>');
-    res.end();
+    //send a html file
+    fs.readFile('./views/index.html', (err, data) => {
+        if (err) {
+            console.log(err);
+            res.end();
+        } else {
+            //res.write(data);
+            res.end(data);
+        }
+    });
 });
 
 server.listen(3000, 'localhost',()=>{
