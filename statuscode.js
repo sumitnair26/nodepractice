57beeb0b2cd5f69550015bbab6bc954e09b0697a
Sumit Nair <sumitnair@Sumits-MacBook-Pro.local>
2023-03-23 14:36:04 +0530

    Status Codes

---
+++ statuscode.js
@@ -0,0 +1,16 @@
+/*
+
+    Status Code 
+
+    200 - OK
+    301 - Resource moved
+    404 - Not found
+    500 - Internal server error 
+
+    100 Range - informational responses
+    200 Range - sucess codes
+    300 Range - code for redirects
+    400 Range - user or client error codes
+    500 Range - server error code 
+
+*/
