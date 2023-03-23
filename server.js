2e679a631f51f5bcca12c37c79123758c3e071e7
Sumit Nair <sumitnair@Sumits-MacBook-Pro.local>
2023-03-23 14:01:58 +0530

    Node Routing to different HTML Pages

--- server.js
+++ server.js
@@ -9,8 +9,21 @@ const server = http.createServer((req, res)=>{
     // res.write('<p>Hello again on landing page</p>');
     // res.end();
 
+    let path = './views/';
+    switch(req.url){
+        case '/':
+            path += 'index.html';
+            break;
+        case '/about':
+            path += 'about.html';
+            break;
+        default :
+            path += '404.html'
+            break;
+    }
+
     //send a html file
-    fs.readFile('./views/index.html', (err, data) => {
+    fs.readFile(path, (err, data) => {
         if (err) {
             console.log(err);
             res.end();
