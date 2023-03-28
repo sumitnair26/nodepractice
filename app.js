44bbbd7735997e2fa9774624b1b0895ab12344c4
Sumit Nair <sumitnair@Sumits-MacBook-Pro.local>
2023-03-28 21:13:26 +0530

    Pass data through EJS

--- app.js
+++ app.js
@@ -14,13 +14,13 @@ app.listen(3000);
 app.get('/', (req, res) => {
     //res.send('<p> Home </p>');
     //res.sendFile('./views/index.html', { root: __dirname });
-    res.render('index');
+    res.render('index', { title: 'Home'});
 });
 
 app.get('/about', (req, res) => {
     //res.send('<p> About </p>');
     //res.sendFile('./views/about.html', { root: __dirname });
-    res.render('about');
+    res.render('about', { title: 'About'});
 });
 
 //redirects
@@ -29,11 +29,11 @@ app.get('/about-us', (req, res) => {
 });
 
 app.get('/blogs/create', (req, res)=> {
-    res.render('create');
+    res.render('create', { title: 'Create'});
 });
 
 //404 pages
 app.use((req, res)=>{
     //res.sendFile('./views/404.html', { root: __dirname });
-    res.status(404).render('404');
+    res.status(404).render('404', { title: '404'});
 });
