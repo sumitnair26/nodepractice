6cf605af7dc2901e1e86b45a159d87b7768bfb7a
Sumit Nair <sumitnair@Sumits-MacBook-Pro.local>
2023-03-28 21:04:03 +0530

    create EJS files

--- app.js
+++ app.js
@@ -1,28 +1,39 @@
+const { application } = require('express');
 const express = require('express');
 const { readdirSync } = require('fs');
 
 //express app
 const app = express();
 
+//register view engine
+app.set('view engine', 'ejs');
+
 //listen for requests
 app.listen(3000);
 
 app.get('/', (req, res) => {
     //res.send('<p> Home </p>');
-    res.sendFile('./views/index.html', { root: __dirname });
+    //res.sendFile('./views/index.html', { root: __dirname });
+    res.render('index');
 });
 
 app.get('/about', (req, res) => {
     //res.send('<p> About </p>');
-    res.sendFile('./views/about.html', { root: __dirname });
+    //res.sendFile('./views/about.html', { root: __dirname });
+    res.render('about');
 });
 
 //redirects
 app.get('/about-us', (req, res) => {
     res.redirect('/about');
-})
+});
+
+app.get('/blogs/create', (req, res)=> {
+    res.render('create');
+});
 
 //404 pages
 app.use((req, res)=>{
-    res.sendFile('./views/404.html', { root: __dirname });
+    //res.sendFile('./views/404.html', { root: __dirname });
+    res.status(404).render('404');
 });
